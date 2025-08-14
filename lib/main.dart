bc190bf58804fa04861c163845a4ba625480e42b
Umar Tanwar <2020pceitmohammed20@poornima.org>
2025-08-14 16:32:37 +0530

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:realtime_chat_flutter/splashscreen.dart';
 import 'package:realtime_chat_flutter/utils/constants.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 
 import 'cubits/profiles/profiles_cubit.dart';
@@ -10,9 +11,11 @@ import 'cubits/profiles/profiles_cubit.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName: ".env");
+
   await Supabase.initialize(
-    url: 'https://xnknmrymfwrpjzuakpsd.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhua25tcnltZndycGp6dWFrcHNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxNDk4MDUsImV4cCI6MjA3MDcyNTgwNX0.cvnnOGR9k_sCaLsXHlYFuRaNY8CiW0sDpSlYbKOo7pQ',
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
 
   runApp(const MyApp());
